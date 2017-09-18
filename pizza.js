4fd91d7f57d017588ca7a9837a2e6f63d530adbc
Chad Rogers <cr0537724@otc.edu>
2017-09-18 13:05:27 -0500

    pizza

---
+++ pizza.js
@@ -0,0 +1,15 @@
+var inquirer = require('inquirer');
+const chalk = require('chalk')
+
+console.log(chalk.red('Order your pizza here'))
+
+class Pizza{
+    constructor(size, cheese, sauce, toppings, cut){
+        this.size=(asize)
+        this.cheese=(extra)
+        this.sauce=(asauce)
+        this.toppings=(atoppings)
+        this.cut=(acut)
+    }
+}
+inquirer.prompt(questions) > promise
